65cb7142ebe2f60bfca2eb7bd1b5451651c51057
lanasanke <mhmmd.maulanna@gmail.com>
2025-05-04 22:24:09 +0800

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -129,4 +129,3 @@ STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS= [""]
